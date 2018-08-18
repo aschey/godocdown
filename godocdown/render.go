22536252c2ee3929dcccc7349f2ba766fcce9605
Roi Avinoam <avinoamr@gmail.com>
2018-08-17 21:59:26 -0700

    Consistency with godoc

--- godocdown/render.go
+++ godocdown/render.go
@@ -48,10 +48,9 @@ func renderExample(w io.Writer, ex *doc.Example) {
 	code := sourceOfNode(ex.Code)
 	code = indentCode(code)
 
-	name, sub := exampleNames(ex.Name)
-	fmt.Fprintf(w, "<a name='Example%s'></a><details><summary>Example%s%s</summary><p>\n\n%s\n%s\n\nOutput:\n```\n%s```\n</p></details>\n\n",
+	_, sub := exampleNames(ex.Name)
+	fmt.Fprintf(w, "<a name='Example%s'></a><details><summary>Example%s</summary><p>\n\n%s\n%s\n\nOutput:\n```\n%s```\n</p></details>\n\n",
 		ex.Name,
-		name,
 		sub,
 		filterText(ex.Doc),
 		code,
@@ -158,7 +157,7 @@ func renderExampleIndexTo(w io.Writer, list []*doc.Example) {
 	fmt.Fprintf(w, "\n#### Examples\n\n")
 	for _, e := range list {
 		name, sub := exampleNames(e.Name)
-		fmt.Fprintf(w, " - [type %s%s](#Example%s)\n", name, sub, e.Name)
+		fmt.Fprintf(w, " - [%s%s](#Example%s)\n", name, sub, e.Name)
 	}
 }
 
